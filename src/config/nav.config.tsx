ec869a5a03781910613610812b21e1a7a564b648
bassem <bassem.gehad95@yahoo.com>
2025-07-13 16:16:46 +0300

    Edit: removed heroIcons and used lucid icons for unified usage of icons through the project.

--- src/config/nav.config.tsx
+++ src/config/nav.config.tsx
@@ -1,40 +1,29 @@
-import {
-  HomeIcon as HomeSolid,
-  HeartIcon as HeartSolid,
-  BellIcon as BellSolid,
-  ChatBubbleOvalLeftEllipsisIcon as MessageSolid,
-} from "@heroicons/react/24/solid";
-import {
-  HomeIcon as HomeOutlined,
-  HeartIcon as HeartOutlined,
-  BellIcon as BellOutlined,
-  ChatBubbleOvalLeftEllipsisIcon as MessageOutlined,
-} from "@heroicons/react/24/outline";
 
+import { Heart, Home , MessageCircleMore ,  BellDot } from "lucide-react";
 const navItems = [
   {
     name: "Home",
     href: "/",
-    iconFilled: <HomeSolid className="w-6 h-6" />,
-    iconOutline: <HomeOutlined className="w-6 h-6" />,
+    iconFilled: <Home fill="currentColor" className="w-6 h-6" />,
+    iconOutline: <Home  className="w-6 h-6" />,
   },
   {
     name: "Messages",
     href: "/profile",
-    iconFilled: <MessageSolid className="w-6 h-6" />,
-    iconOutline: <MessageOutlined className="w-6 h-6" />,
+    iconFilled: <MessageCircleMore fill="currentColor" className="w-6 h-6" />,
+    iconOutline: <MessageCircleMore  className="w-6 h-6" />,
   },
   {
     name: "Notifications",
     href: "/profile",
-    iconFilled: <BellSolid className="w-6 h-6" />,
-    iconOutline: <BellOutlined className="w-6 h-6" />,
+    iconFilled: <BellDot fill="currentColor" className="w-6 h-6" />,
+    iconOutline: <BellDot  className="w-6 h-6" />,
   },
   {
     name: "Profile",
     href: "/profile",
-    iconFilled: <HeartSolid className="w-6 h-6" />,
-    iconOutline: <HeartOutlined className="w-6 h-6" />,
+    iconFilled: <Heart fill="currentColor" className="w-6 h-6" />,
+    iconOutline: <Heart  className="w-6 h-6" />,
   },
 ];
 
