40e87f164aa5f763b1792cce92749c2ff2a54948
mvicsa <mvicsa@yahoo.com>
2025-07-16 02:07:05 +0300

    Update UserReaction interface to change userId type from User object to string for compatibility

--- src/types/post.ts
+++ src/types/post.ts
@@ -1,7 +1,7 @@
 import type { User } from './user';
 
 export interface UserReaction {
-  userId: User; // now a populated user object!
+  userId: string; // changed from User to string for compatibility
   username: string;
   reaction: string;
   createdAt: string;
